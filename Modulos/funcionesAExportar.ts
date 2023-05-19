ff151e7447bb87c0d41fe835fb8e2da305f0a616
Yair <yairvelascoramirez@gmail.com>
2023-05-19 10:53:25 -0600

    agregando carpet Modulos

---
+++ Modulos/funcionesAExportar.ts
@@ -0,0 +1,13 @@
+function suma(a:number, b:number):number{
+    return a+b;
+}
+
+function restar(a:number, b:number):number{
+    return a-b;
+}
+
+
+export let obj={
+    suma,
+    restar
+}
---
+++ Modulos/main.ts
@@ -0,0 +1,3 @@
+import {obj} from "./funcionesAExportar";
+
+console.log(obj.suma(10,4))
